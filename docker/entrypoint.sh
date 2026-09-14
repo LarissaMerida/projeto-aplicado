2676af105e1a05d0f5f8ca1f2f743496fd90050b
Larissa <larissadasilvasantos746@gmail.com>
2026-09-13 23:03:41 -0300

    fix: Correct gunicorn command by adding missing --chdir option

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 python src/manage.py migrate --noinput
-exec gunicorn projeto_aplicado.wsgi:application \
+exec gunicorn --chdir src projeto_aplicado.wsgi:application \
 	--bind 0.0.0.0:8000 \
 	--workers "${GUNICORN_WORKERS:-2}" \
 	--timeout "${GUNICORN_TIMEOUT:-60}"
